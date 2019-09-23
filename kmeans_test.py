f9c6052209bb0f96eec39913a551b0ac67491b7f
premi25 <p.siekaniec@interia.pl>
2019-09-23 20:10:45 +0200

    Commit changes in kmeans_test.py

--- kmeans_test.py
+++ kmeans_test.py
@@ -17,19 +17,18 @@ from pyclustering.cluster.silhouette import silhouette
 from pyclustering.utils import read_sample
 from pyclustering.utils.metric import type_metric, distance_metric
 #-------------------------------------------------------------------
-from operationsOnFile import writeListItemsToCSV as wlitCSV
-from operationsOnFile import writeItemToCSV as witCSV
+#from operationsOnFile import writeListItemsToCSV as wlitCSV
+#from operationsOnFile import writeItemToCSV as witCSV
 #-------------------------------------------------------------------
 from sklearn.metrics import davies_bouldin_score as dbs
 from sklearn.metrics import calinski_harabasz_score as chs
 
 root = pathlib.Path('F:\\US_18_19\PracaMag\Data')
 filenameData = 'dataReadyForClusteringWork.txt'
-filenameSilhouette = 'silhouette1.csv'
-filenameSilhouetteMean = 'silhouette_mean.csv'
-filenameDBS = 'dbs.csv'
-filenameCHS = 'chs.csv'
-#filename
+filenameSilhouette = 'silhouetteMeans.csv'
+filenameSilhouetteMean = 'silhouetteMeans_mean.csv'
+filenameDBS = 'dbsMeans.csv'
+filenameCHS = 'chsMeans.csv'
 k = 10
 k_min, k_max = 1, 15
 metric = distance_metric(type_metric.EUCLIDEAN)
@@ -45,31 +44,22 @@ def kmeansWithScores(nameData, nameSilhouetteMean, nameDBS, nameCHS, k_clusters,
     predicted = kmeans_instance.predict(data)
 
 #    final_centers = kmeans_instance.get_centers()
-#kv.show_clusters(data, clusters, final_centers) #too many dimensions
 
-#visualizer = cluster_visualizer_multidim()
-#visualizer.append_clusters(clusters, data)
-#visualizer.show(pair_filter=[[0, 7]])
-
-#from sklearn.cluster import KMeans
-#kmeans_model = KMeans(n_clusters=3, random_state=1).fit(X)
     silhouetteScore = silhouette(data, clusters).process().get_score()
     meanSilhouetteScore = np.mean(silhouetteScore)
-    wlitCSV(silhouetteScore, filenameSilhouette, '', root)
-    witCSV(meanSilhouetteScore, nameSilhouetteMean, '', root)
+    #wlitCSV(silhouetteScore, filenameSilhouette, '', root)
+    #witCSV(meanSilhouetteScore, nameSilhouetteMean, '', root)
 
     dbsScore = dbs(data, predicted)
-    witCSV(dbsScore, nameDBS, '', root)
+    #witCSV(dbsScore, nameDBS, '', root)
 
     chsScore = chs(data, predicted)
-    witCSV(chsScore, nameCHS, '', root)
+    #witCSV(chsScore, nameCHS, '', root)
 
     elbow_instance = elbow(data, kmin, kmax)
     elbow_instance.process()
-    amount_clusters = elbow_instance.get_amount()  # most probable amount of clusters
+    amount_clusters = elbow_instance.get_amount()
     wce = elbow_instance.get_wce()
 
-    print ("amount_cluster" + str(amount_clusters))
-    print ("wce" + str(wce))
 
 kmeansWithScores(filenameData, filenameSilhouetteMean, filenameDBS, filenameCHS, k, metric, k_min, k_max)
